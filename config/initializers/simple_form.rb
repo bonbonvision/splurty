d35fe4457e5dbb2b8a6b6014f7d0583105eae7fb
Bonnie Kilpatrick <kilpatrick.bonnie@gmail.com>
2017-03-26 19:40:29 +0000

    Created About page and added content

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -166,4 +166,8 @@ SimpleForm.setup do |config|
 
   # Defines which i18n scope will be used in Simple Form.
   # config.i18n_scope = 'simple_form'
+  
+  # Removes the stars 
+  config.label_text = lambda { |label, required, explicit_label| "#{} #{label}" }
+
 end
